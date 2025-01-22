df4dc03e22cf02f71c12faf875be98f7248c8b01
lucia-mn <lucmennah@alu.edu.gva.es>
2025-01-22 12:11:00 +0100

    Add files via upload

---
+++ und5act2/src/ej1/Pared.java
@@ -0,0 +1,21 @@
+package ej1;
+
+public class Pared {
+
+    //atributo
+    private String dirección;
+
+    //constructor
+    public Pared(String dirección) {
+        this.dirección = dirección;
+    }
+
+    //getters y setters
+    public String getDirección() {
+        return dirección;
+    }
+
+    public void setDirección(String dirección) {
+        this.dirección = dirección;
+    }
+}
