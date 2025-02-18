5e70c0539d78b78d1a3f98be97247312084bd788
lucia-mn <lucmennah@alu.edu.gva.es>
2025-02-18 11:49:23 +0100

    Add files via upload

--- intermodular/src/intermodularColeccionDatos/mainResena.java
+++ intermodular/src/intermodularColeccionDatos/mainResena.java
@@ -10,125 +10,123 @@ import java.util.Scanner;
 
 public class mainResena {
 
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        ArrayList<Resena> resenas = new ArrayList<>();
+        public static void main(String[] args) {
+            Scanner scanner = new Scanner(System.in);
+            ArrayList<Resena> resenas = new ArrayList<>();
 
-        boolean fin = false;
-        while (!fin) {
-            System.out.println(" ");
-            System.out.println("*** Introducción de datos de la reseña ***");
-            System.out.println(" ");
+            boolean fin = false;
+            while (!fin) {
+                System.out.println(" ");
+                System.out.println("*** Introducción de datos de la reseña ***");
+                System.out.println(" ");
 
-            String idResena;
-            do {
-                System.out.print("ID de la reseña, máximo 64 caracteres: ");
-                idResena = scanner.nextLine();
-            } while (idResena.length() > 64);
-
-
-            int idProducto;
-            do {
-                System.out.print("ID del producto, solo números: ");
-                idProducto = obtenerNumero(scanner);
-            } while (idProducto <= 0);
+                String idResena;
+                do {
+                    System.out.print("ID de la reseña, máximo 64 caracteres: ");
+                    idResena = scanner.nextLine();
+                } while (idResena.length() > 64);
 
 
-            int calificacion;
-            do {
-                System.out.print("Calificación del 1 al 10, solo números enteros: ");
-                calificacion = obtenerNumero(scanner);
-            } while (calificacion < 1 || calificacion > 10);
+                int idProducto;
+                do {
+                    System.out.print("ID del producto, solo números: ");
+                    idProducto = obtenerNumero(scanner);
+                } while (idProducto <= 0);
 
 
-            String contenido;
-            do {
-                System.out.print("Contenido de la reseña, máximo 100 caracteres: ");
-                contenido = scanner.nextLine();
-            } while (contenido.length() > 100);
+                int calificacion;
+                do {
+                    System.out.print("Calificación del 1 al 10, solo números enteros: ");
+                    calificacion = obtenerNumero(scanner);
+                } while (calificacion < 1 || calificacion > 10);
 
 
-            String fPublicacion;
-            do {
-                System.out.print("Fecha de publicación de la reseña en formato AAAA-MM-DD: ");
-                fPublicacion = scanner.nextLine();
-            } while (!esFechaValida(fPublicacion));
+                String contenido;
+                do {
+                    System.out.print("Contenido de la reseña, máximo 100 caracteres: ");
+                    contenido = scanner.nextLine();
+                } while (contenido.length() > 100);
 
 
-            int idUsuario;
-            do {
-                System.out.print("ID del usuario, solo números: ");
-                idUsuario = obtenerNumero(scanner);
-            } while (idUsuario <= 0);
+                String fPublicacion;
+                do {
+                    System.out.print("Fecha de publicación de la reseña en formato AAAA-MM-DD: ");
+                    fPublicacion = scanner.nextLine();
+                } while (!esFechaValida(fPublicacion));
 
-            //añadir reseñas
-            Resena resena = new Resena(idResena, idProducto, calificacion, contenido, fPublicacion, idUsuario);
-            resenas.add(resena);
-            System.out.println("Reseña añadida correctamente");
 
-            mostrarResenas(resenas);
+                int idUsuario;
+                do {
+                    System.out.print("ID del usuario, solo números: ");
+                    idUsuario = obtenerNumero(scanner);
+                } while (idUsuario <= 0);
 
-            System.out.println(" ");
-            System.out.print("¿Quieres añadir otra reseña más? Pulsa cualquier tecla para continuar o escribe fin para salir: ");
-            String respuesta = scanner.nextLine().toLowerCase();
-            if (respuesta.equals("fin")) {
-                fin = true;
+                //añadir reseñas
+                Resena resena = new Resena(idResena, idProducto, calificacion, contenido, fPublicacion, idUsuario);
+                resenas.add(resena);
+                System.out.println("Reseña añadida correctamente");
+
+                mostrarResenas(resenas);
+
+                System.out.println(" ");
+                System.out.print("¿Quieres añadir otra reseña más? Pulsa cualquier tecla para continuar o escribe fin para salir: ");
+                String respuesta = scanner.nextLine().toLowerCase();
+                if (respuesta.equals("fin")) {
+                    fin = true;
+                }
+            }
+
+            System.out.println("*** Fin del programa ***");
+        }
+
+
+
+        //validar fecha
+        private static boolean esFechaValida(String fecha) {
+            SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
+            sdf.setLenient(false);
+            try {
+                sdf.parse(fecha);
+                return true;
+
+            } catch (ParseException e) {
+                System.out.println("Formato incorrecto, vuelve a intentarlo: ");
+                return false;
             }
         }
 
-        System.out.println("*** Fin del programa ***");
-    }
+        //idproducto, calificaion, idusuario que sean números
+        private static int obtenerNumero(Scanner scanner) {
+            while (true) {
+                try {
+                    return Integer.parseInt(scanner.nextLine());
+
+                } catch (NumberFormatException e) {
+                    System.out.print("Tiene que ser un número, vuelve a intentarlo: ");
+                }
+            }
+        }
+
+        //imprimir array
+        private static void mostrarResenas(ArrayList<Resena> resenas) {
+            System.out.println(" ");
+            System.out.println("Lista de reseñas: ");
+            for (Resena r : resenas) {
+                System.out.println(r);
+            }
+        }
 
 
-    //validar fecha
-    private static boolean esFechaValida(String fecha) {
-        SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
-        sdf.setLenient(false);
-        try {
-            sdf.parse(fecha);
-            return true;
-
-        } catch (ParseException e) {
-            System.out.println("Formato incorrecto, vuelve a intentarlo: ");
+        public boolean removeResena(String idResena) {
+            Iterator<Resena> iterator = resenas.iterator();
+            while (iterator.hasNext()) {
+                Resena resena = iterator.next();
+                if (resena.getIdResena().equals(idResena)) {
+                    iterator.remove();
+                    return true;
+                }
+            }
             return false;
         }
-    }
 
-    //idproducto, calificaion, idusuario que sean números
-    private static int obtenerNumero(Scanner scanner) {
-        while (true) {
-            try {
-                return Integer.parseInt(scanner.nextLine());
-
-            } catch (NumberFormatException e) {
-                System.out.print("Tiene que ser un número, vuelve a intentarlo: ");
-            }
-        }
-    }
-
-    //imprimir array
-    private static void mostrarResenas(ArrayList<Resena> resenas) {
-        System.out.println(" ");
-        System.out.println("Lista de reseñas: ");
-        for (Resena r : resenas) {
-            System.out.println(r);
-        }
-    }
-}
-
-
-
-
-
-
-public boolean removeResena(String idResena) {
-    Iterator<Resena> iterator = resenas.iterator();
-    while (iterator.hasNext()) {
-        Resena resena = iterator.next();
-        if (resena.getIdResena().equals(idResena)) {
-            iterator.remove();
-            return true;
-        }
-    }
-    return false;
 }
